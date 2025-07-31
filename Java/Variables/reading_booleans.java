7cd71dbf64184f94208a366d9794b9927cf00cad
Vitória Comiran <126994827+vitoriacomiran@users.noreply.github.com>
2025-07-31 15:10:59 -0300

    Update reading_booleans.java

--- Java/Variables/reading_booleans.java
+++ Java/Variables/reading_booleans.java
@@ -1,6 +1,6 @@
-/* Para converter uma string em um valor booleano deve-se usar Boolean.valueOf().
+// Para converter uma string em um valor booleano deve-se usar Boolean.valueOf().
 
-Booleano aceita valor true e false, qualquer outro valor que não seja true torna a variável falsa. */
+//Booleano aceita valor true e false, qualquer outro valor que não seja true torna a variável falsa. 
 
 import java.util.Scanner;
 
