23ab456e23b36d5243a98588f50da3cece2cb5a0
Vitoria <vitoriacmn@gmail.com>
2025-08-30 22:55:54 -0300

    strict mode

---
+++ JavaScript/js moderno/jsmoderno.js
@@ -0,0 +1,9 @@
+// strict mode
+
+function showMessage(){
+    "use strict" // os erros silenciosos do JS são sinalizados 
+    personName = "Vitória"
+    console.log("Olá", personName)
+}
+
+showMessage()
