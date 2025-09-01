23335c2c44f42ddf7ef2605c596b164364bfc99d
Vitoria <vitoriacmn@gmail.com>
2025-09-01 19:52:03 -0300

    Criação de spread.

---
+++ JavaScript/js moderno/spread.js
@@ -0,0 +1,12 @@
+// espalhar => permite que um objeto iterável, array ou string, seja expandido para ser usado
+// onde zero ou mais argumentos
+
+const numbers = [1, 2, 3]
+console.log(...numbers)
+
+// retorna (3) [1,2,3]
+
+const names = ["Ana", "Maria", "João"]
+console.log(...names)
+
+// retorna (3) ["Ana", "Maria", "João"]
